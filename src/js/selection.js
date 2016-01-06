7f31ffd99dcd0c5df8c2a630df0669e5039458ee
victorrodriguez <vrodriguez5749@gmail.com>
2016-01-06 16:40:00 -0600

    added element selection and creation and fixed inheritance structure for Input

--- src/js/selection.js
+++ src/js/selection.js
@@ -1,23 +1,25 @@
+//TODO: implement off, append, remove, click, extend 
+
 var $ = (function($) {
     ////////////////////////////////////////////////////////////////////////////////////////////
-    /// Utility Methods
+    /// Utility Functions
     ////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      * checks a string for the html format
-     * @param  {string}  str string to test
-     * @return {Boolean}     returns whether string is html
+     * @param  {string}  str - string to test
+     * @return {Boolean}       returns whether string is html
      */
     function isHTML(str) {
-        var htmlRegex = /<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/;
+        var htmlRegex = /<[a-z][\s\S]*>/;
 
         return htmlRegex.test(str);
-
     }
 
     /**
      * Extracts tag from html string
-     * @param  {string} str html string
-     * @return {string}     tag name
+     * @param  {string} str - html string
+     * @return {string}       tag name
      */
     function getTag(str) {
         var strippedTags = str.split('<')[1].split('>');
@@ -28,8 +30,8 @@ var $ = (function($) {
 
     /**
      * Extracts attributes from html string
-     * @param  {string} str html string
-     * @return {Object}     map containing key value pairs of attributes
+     * @param  {string} str - html string
+     * @return {Object}       map containing key value pairs of attributes
      */
     function getAttrs(str) {
         var strippedTags = str.split('<')[1].split('>');
@@ -53,66 +55,107 @@ var $ = (function($) {
     }
     /**
      * Searches DOM using CSS selectors
-     * @param  {string} selector selector to use in search
-     * @return {Object|Object[]|false}          Element or Element[] representing element(s) found or false if no match
+     * @param  {string} selector     - selector to use in search
+     * @return {Object|Object[]|false} Element or Element[] representing element(s) found or false if no match
      */
     function findElement(selector) {
         var results = document.querySelectorAll(selector);
+        var multipleResults = [];
         if (results.length === 0) {
             return false;
         } else if (results.length > 1) {
+            for (var i = 0; i < results.length; i++) {
+                multipleResults.push(createExistingElement(results[i]));
+            }
 
+            return multipleResults;
         } else {
 
+            return createExistingElement(results[0]);
         }
+    }
+
+    /**
+     * Uses node to create Element
+     * @param  {Object} node 	  - Node to use in object creation
+     * @return {Element|Input}      Input for input or select elements, Element for all others
+     */
+    function createExistingElement(node) {
+        var tag = node.tagName;
+        var attrs = {};
+        var existingElement;
+        for (var attr in node.attributes) {
+            if (node.attributes.hasOwnProperty(attr)) {
+                attrs[node.attributes[attr].name] = node.attributes[attr].value;
+            }
+        }
+        if (tag === 'input' || tag === 'select') {
+            existingElement = new Input(attrs);
+        } else {
+            existingElement = new Element(tag, attrs, node);
+        }
+
+        return existingElement;
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////
     /// Objects
     ////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      * Parent class for general elements
-     * @param {string} tag   tag name
-     * @param {Object} attrs map of attributes and values
+     * @param {string} tag   - tag name
+     * @param {Object} attrs - map of attributes and values
+     * @param {Object} node  - node to initialize object with
      */
     function Element(tag, attrs, node) {
         this.tag = tag;
         this.attrs = attrs;
-        this.listeners = {};
+        this.listeners = [];
         if (node) {
-        	this.node = node;
+            this.node = node;
         } else {
-        	this.node = this.createNode();
+            this.node = this.createNode();
         }
     }
+    Element.prototype.eventTypes = ['click', 'blur', 'hover', 'keyup', 'focus', 'keydown', 'mouseup', 'mousedown', 'mouseleave', 'scroll'];
     /**
      * add event handler to element
-     * @param  {string} event   event name
-     * @param  {Function} handler function to use on event
-     * @return {Object}         returns element object
+     * @param  {string} event     - event name
+     * @param  {Function} handler - function to use on event
+     * @return {Object}             returns element object
      */
     Element.prototype.on = function(event, handler) {
-    	return this; //method chaining
+        this.node.addEventListener(event, handler);
+        this.listeners.push({
+            event: event,
+            handler: handler
+        });
+
+        return this; //method chaining
     };
     /**
      * remove event handler from element
-     * @param  {string} event   event name
+     * @param  {string} event - event name
      * @return {Object}         returns element object
      */
     Element.prototype.off = function(event) {
-    	return this; //method chaining
+
+        return this; //method chaining
     };
     /**
      * Gets or sets attribute of element
-     * @param  {string} attr  attribute to set or get
-     * @param  {string} value value to change attribute to
-     * @return {Object}       returns Node object
+     * @param  {string} attr  - attribute to set or get
+     * @param  {string} value - value to change attribute to
+     * @return {Object}         returns Node object
      */
     Element.prototype.attr = function(attr, value) {
         if (value) {
             this.attrs[attr] = value;
+
             return this; //method chaining
         } else {
+
             return this.attrs[attr];
         }
     };
@@ -124,43 +167,46 @@ var $ = (function($) {
         var node = document.createElement(this.tag);
         for (var key in this.attrs) {
             if (this.attrs.hasOwnProperty(key)) {
-            	node.setAttribute(key, this.attrs[key]);
+                node.setAttribute(key, this.attrs[key]);
             }
         }
+
         return node;
     };
     /**
      * Gets the node representation of the object
      * @return {Object} Node
      */
-    Element.prototype.getNode = function () {
-    	return this.node;
+    Element.prototype.getNode = function() {
+
+        return this.node;
     };
     /**
      * Constructor for Input type. Inherits from Element
-     * @param {Object} attrs attributes to pass to parent
+     * @param {Object} attrs - attributes to pass to parent
      */
     function Input(attrs) {
         Element.call(this, 'input', attrs);
-    }
-    /**
-     * Add event handler to element overridden from parent to add input and change event
-     * @param  {string} event   event name
-     * @param  {Function} handler function to use on event
-     * @return {Object}         returns element object
-     */
-    Input.prototype.on = function(event, handler) {
+        this.value = '';
+        Input.prototype.eventTypes = this.eventTypes.concat(['input', 'change']);
 
-    };
+        this.node.addEventListener('keyup', function (e) {
+        	this.value = e.target.value;
+        }.bind(this));
+
+    }
+
+
+    Input.prototype = Object.create(Element.prototype);
 
     ////////////////////////////////////////////////////////////////////////////////////////////
     /// Inititializers
     ////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
-     * Element creation or searching
-     * @param  {string} str html string or selector
-     * @return {Object|Object[]|false}     Object, Object[] or false for search and Element if creating
+     * Element creation or searching (does not yet support nested elements)
+     * @param  {string} str          - html string or selector
+     * @return {Object|Object[]|false} one or more objects or false for search and Element if creating
      */
     $.elem = function(str) {
         var attrs;
@@ -168,13 +214,16 @@ var $ = (function($) {
         if (isHTML(str)) {
             if (getTag(str) === 'input') {
                 attrs = getAttrs(str);
+
                 return new Input(attrs);
             } else {
                 tag = getTag(str);
                 attrs = getAttrs(str);
+
                 return new Element(tag, attrs);
             }
         } else {
+
             return findElement(str);
         }
     };
@@ -182,15 +231,44 @@ var $ = (function($) {
     ////////////////////////////////////////////////////////////////////////////////////////////
     /// Test Code : stripped out during build
     ////////////////////////////////////////////////////////////////////////////////////////////
+
     /* start-test */
     $.test = {
         Mimic: 'some test code',
         isHTML: isHTML,
         getTag: getTag,
         getAttrs: getAttrs,
-        Element: Element
+        Element: Element,
+        Input: Input
     };
+    //bind polyfill - PhantomJS does not include bind()
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function(oThis) {
+            if (typeof this !== 'function') {
+                // closest thing possible to the ECMAScript 5
+                // internal IsCallable function
+                throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function() {},
+                fBound = function() {
+                    return fToBind.apply(this instanceof fNOP ? this : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            if (this.prototype) {
+                // native functions don't have a prototype
+                fNOP.prototype = this.prototype;
+            }
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    }
     /* end-test */
 
     return $;
-}($ || {}));
+}($ || {})); // Take in $ as a dependency. If it has already been declared, add to it,
+// if not, create it. Now it doesn't matter what order you load the modules.
--- tests/selectionMethods.js
+++ tests/selectionMethods.js
@@ -2,9 +2,11 @@ describe('selection', function() {
     describe('isHTML', function() {
         it('should recognize html', function() {
             expect($.test.isHTML('<div></div>')).toBe(true);
+            expect($.test.isHTML('<input type="text" id="test">')).toBe(true);
         });
         it('should detect invalid html', function() {
             expect($.test.isHTML('</div>')).toBe(false);
+            expect($.test.isHTML('h1')).toBe(false);
         });
         it('should detect strings', function() {
             expect($.test.isHTML('this is a string')).toBe(false);
@@ -39,6 +41,42 @@ describe('selection', function() {
         it('should return an Element object', function() {
             expect($elem).toEqual(jasmine.any($.test.Element));
         });
+        describe('finding existing element', function () {
+        	var testElement, testElementTwo;
+
+        	beforeEach(function () {
+        		testElement = document.createElement('div');
+        		testElement.setAttribute('class', 'myClass');
+        		testElement.setAttribute('id', 'testId');
+
+        		testElementTwo = document.createElement('div');
+        		testElementTwo.setAttribute('class', 'myClass');
+        		testElementTwo.setAttribute('id', 'testIdTwo');
+
+        		document.body.appendChild(testElement);
+        		document.body.appendChild(testElementTwo);
+        	});
+        	afterEach(function () {
+        		document.body.removeChild(testElement);
+        		document.body.removeChild(testElementTwo);
+        	});
+        	it('should find element on the page and create an Element', function () {
+        		var result = $.elem('#testId');
+        		expect(result).toEqual(jasmine.any($.test.Element));
+        		expect(result.getNode()).toEqual(jasmine.any(Node));
+        		expect(testElement).toEqual(result.getNode());
+        	});
+        	it('should get an array for multiple elements', function () {
+        		var result = $.elem('.myClass');
+        		expect(result.length > 0).toBe(true);
+        		expect(result[0]).toEqual(jasmine.any($.test.Element));
+        	});
+        	it('should pull in all attributes from existing element', function () {
+        		var result = $.elem('#testId');
+        		expect(result.attr('id')).toBe('testId');
+        		expect(result.attr('class')).toBe('myClass');
+        	});
+        });
         describe('attr', function() {
         	it('should return correct id', function () {
         		expect($elem.attr('id')).toBe('myId');
@@ -60,6 +98,15 @@ describe('selection', function() {
         		var node = $elem.getNode();
         		expect(node.id).toBe('myId');
         		expect(node.getAttribute('class')).toBe('myClass');
+        	});
+        });
+
+        describe('Input', function () {
+        	it('should have different event types', function () {
+        		var e = new $.test.Element('div', {class:'myClass'});
+        		var i = new $.test.Input({class:'myClass'});
+        		expect(e.eventTypes.indexOf('input') === -1).toBe(true);
+        		expect(i.eventTypes.indexOf('input') > -1).toBe(true);
         	});
         });
     });
