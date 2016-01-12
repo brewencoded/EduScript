c93334118a213f49f45151b2b4636856fdd27df2
victorrodriguez <vrodriguez5749@gmail.com>
2016-01-12 16:50:04 -0600

    added basic functionality and test structure

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,10 +1,53 @@
-var $ = (function ($) {
+var $ = (function($) {
 
-	function Ajax (selector) {
-		
-	}
+    function ajax(options, callback) {
+        var xmlhttp;
 
-	$.ajax = new Ajax();
-	
-	return $;
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp.status == 200) {
+                    callback(xmlhttp);
+                } else if (xmlhttp.status == 404) {
+                    callback(null, xmlhttp);
+                } else {
+                    callback(null, xmlhttp);
+                }
+            }
+        };
+
+        xmlhttp.open(options.method, options.url, true);
+        xmlhttp.send(options.data);
+    }
+
+
+
+    $.ajax = ajax;
+
+    $.get = function (url, callback) {
+    	var options = {
+    		url: url,
+    		method: 'GET',
+    		data: null
+    	};
+    	$.ajax(options, callback);
+    };
+
+    $.post = function (url, data, callback) {
+    	var options = {
+    		url: url,
+    		method: 'POST',
+    		data: data
+    	};
+    	$.ajax(options, callback);
+    };
+
+    return $;
 }($ || {}));
