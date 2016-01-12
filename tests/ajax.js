1f28baffe805337e278894e09652d80ad5e7c51b
victorrodriguez <vrodriguez5749@gmail.com>
2016-01-12 16:50:58 -0600

    added basic functionality and test structure

---
+++ tests/ajax.js
@@ -0,0 +1,9 @@
+describe('ajax', function() {
+	jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+    describe('$.ajax', function() {
+
+        it('should be able to get data', function() {
+
+        });
+    });
+});
