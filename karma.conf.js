93b007befcfa640356c7693f2ce01be4012bb7e6
victorrodriguez <vrodriguez5749@gmail.com>
2016-01-13 16:50:38 -0600

    added more thorough testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,21 @@
 'use strict';
 // karma.conf.js
 module.exports = function(config) {
-  config.set({
-    frameworks: ['jasmine'],
-    reporters: ['spec'],
-    browsers: ['PhantomJS'],
-    files: [
-      'src/js/**/*.js',
-      'tests/*.js'
-    ]
+    config.set({
+        frameworks: ['jasmine-ajax', 'jasmine'],
+        reporters: ['spec'],
+        browsers: ['PhantomJS'],
+        files: [
+            'src/js/**/*.js',
+            'spec/tests/*.js', {
+                pattern: 'spec/fixtures/*.json',
+                served: true,
+                included: false
+            }
+        ],
+        proxies: {
+            '/spec/fixtures/': '/base/spec/fixtures/'
+        }
 
-  });
+    });
 };
--- tests/ajax.js
+++
@@ -1,9 +0,0 @@
-describe('ajax', function() {
-	jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
-    describe('$.ajax', function() {
-
-        it('should be able to get data', function() {
-
-        });
-    });
-});
