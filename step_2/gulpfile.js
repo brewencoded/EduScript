9ffaff322f2018da7f4c54fc998d7e13c5f5bab9
victorrodriguez <vrodriguez5749@gmail.com>
2016-01-19 16:45:30 -0600

    browserify and typescript workflow added

--- step_2/gulpfile.js
+++ step_2/gulpfile.js
@@ -14,34 +14,34 @@ var gulp = require('gulp'),
     stripCode = require('gulp-strip-code'),
     rename = require('gulp-rename'),
     yargs = require('yargs').argv,
-    ts = require('gulp-typescript');
+    source = require('vinyl-source-stream'),
+    browserify = require('browserify'),
+    tsify = require('tsify');
 
 ///////////////////////////////////////////////////
 /// Constants
 ///////////////////////////////////////////////////
 var src = {
-        ts: '',
+        ts: 'src/ts',
         sass: '',
         index: ''
     },
     dest = {
-        js: '',
+        js: 'build/js',
         css: '',
         index: ''
     };
-    
+
 ///////////////////////////////////////////////////
 /// TypeScript
 ///////////////////////////////////////////////////
-gulp.task('ts', function() {
-    gulp.src(['src/ts/**/*.ts'])
-        .pipe(plumber())
-        .pipe(ts({
-			noImplicitAny: true,
-			out: 'app.js',
-            target: 'es5',
-            removeComments: true
-		}))
+gulp.task('ts', function () {
+    var bundler = browserify()
+        .add(src.ts + '/main.ts')
+        .plugin(tsify);
+        
+    return bundler.bundle()
+        .pipe(source('app.js'))
         .pipe(gulp.dest('build/js'));
 });
 
@@ -50,7 +50,7 @@ gulp.task('ts-watch', ['ts'], browserSync.reload);
 ///////////////////////////////////////////////////
 /// Sass
 ///////////////////////////////////////////////////
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src('src/scss/style.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
@@ -66,7 +66,7 @@ gulp.task('sass', function() {
 ///////////////////////////////////////////////////
 /// HTML
 ///////////////////////////////////////////////////
-gulp.task('html', function() {
+gulp.task('html', function () {
     return gulp.src('src/index.html')
         .pipe(plumber())
         .pipe(gulp.dest('build/'));
@@ -76,7 +76,7 @@ gulp.task('html', function() {
 /// Watch and live reload
 ///////////////////////////////////////////////////
 gulp.task('browser-sync', ['sass', 'ts', 'html'], function () {
-	browserSync.init({
+    browserSync.init({
         server: './build',
         port: 8087
     });
